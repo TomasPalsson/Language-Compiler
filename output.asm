0741604da70ba1fdf1a3e8d5b63090738a2017b7
Tomas Palsson <tomas2212042710@gmail.com>
2025-04-16 00:54:30 +0000

    Fix output

--- output.asm
+++ output.asm
@@ -1,6 +1,9 @@
 extern _printf
 section .rodata
 fmt: db "%ld", 10, 0
+fmt_str: db "%s", 10, 0
+str_0: db "Its a 5", 0
+str_1: db "B is less than 5", 0
 global _main
 section .text
 _main:
@@ -9,52 +12,76 @@ _main:
     sub rsp, 24
     mov rax, 1
     mov [rbp - 8], rax
-    mov rax, 2
-    mov [rbp - 16], rax
+while_start_0:
     mov rax, [rbp - 8]
     push rax
-    mov rax, [rbp - 16]
+    mov rax, 5
     pop rcx
-    add rax, rcx
-    mov [rbp - 24], rax
-    mov rsi, [rbp - 24]
+    cmp rax, rcx
+    setg al
+    movzx rax, al
+    cmp rax, 0
+    je while_end_1
+    mov rsi, [rbp - 8]
     lea rdi, [rel fmt]
     mov rax, 0
     call _printf
-    mov rax, [rbp - 24]
+    call _otherwhile
+    mov rax, [rbp - 8]
     push rax
-    mov rax, 3
+    mov rax, 1
+    pop rcx
+    add rax, rcx
+    mov [rbp - 8], rax
+    mov rax, [rbp - 8]
+    push rax
+    mov rax, 5
     pop rcx
     cmp rax, rcx
     sete al
     movzx rax, al
     cmp rax, 0
-    je else_1
-    mov rsi, [rbp - 8]
-    lea rdi, [rel fmt]
+    je endif_2
+    lea rsi, [rel str_0]
+    lea rdi, [rel fmt_str]
     mov rax, 0
     call _printf
-    jmp endif_0
-else_1:
-    mov rsi, [rbp - 16]
-    lea rdi, [rel fmt]
-    mov rax, 0
-    call _printf
-endif_0:
-    call _func
+endif_2:
+    jmp while_start_0
+while_end_1:
     mov rsp, rbp
     mov rax, 0
     pop rbp
     ret
-global _func
-_func:
+global _otherwhile
+_otherwhile:
     push rbp
     mov rbp, rsp
     sub rsp, 24
-   mov rsi, 1111
-    lea rdi, [rel fmt]
+    mov rax, 1
+    mov [rbp - 16], rax
+while_start_3:
+    mov rax, [rbp - 16]
+    push rax
+    mov rax, 5
+    pop rcx
+    cmp rax, rcx
+    setg al
+    movzx rax, al
+    cmp rax, 0
+    je while_end_4
+    lea rsi, [rel str_1]
+    lea rdi, [rel fmt_str]
     mov rax, 0
     call _printf
+    mov rax, [rbp - 16]
+    push rax
+    mov rax, 1
+    pop rcx
+    add rax, rcx
+    mov [rbp - 16], rax
+    jmp while_start_3
+while_end_4:
     mov rsp, rbp
     mov rax, 0
     pop rbp
